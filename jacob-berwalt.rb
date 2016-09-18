4431c1be752aaaa6b4bffc1bcddea4f2954737a6
Marco Heisig <marco.heisig@fau.de>
2016-09-18 15:26:10 +0200

    drop fetch

--- jacob-berwalt.rb
+++ jacob-berwalt.rb
@@ -5,19 +5,6 @@ require "net/http"
 
 sitemap_url = URI.parse('https://de.wikibooks.org/w/index.php?title=Mathe_f%C3%BCr_Nicht-Freaks:_Sitemap&action=raw')
 
-def fetch(uri_str, limit = 10)
-  raise 'HTTP redirect too deep' if limit == 0
+sitemap = Net::HTTP.get(sitemap_url)
 
-  puts uri_str
-  url = URI.parse(uri_str)
-  req = Net::HTTP::Get.new(url.path)
-  response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
-  case response
-  when Net::HTTPSuccess     then response
-  when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-  else
-    response.error!
-  end
-end
-
-puts Net::HTTP.get(sitemap_url)
+puts sitemap.scan(/\[\[.*\]\]/)
