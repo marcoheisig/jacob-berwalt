91f232ba396a8470f205e2108bb96c1df7e99298
Marco Heisig <marco.heisig@fau.de>
2016-09-20 00:05:39 +0200

    honk if you read this commit message :)

--- jacob-berwalt.rb
+++ jacob-berwalt.rb
@@ -1,10 +1,45 @@
 #! /usr/bin/env ruby
 # coding: utf-8
 
+require "open-uri"
 require "net/http"
 
-sitemap_url = URI.parse('https://de.wikibooks.org/w/index.php?title=Mathe_f%C3%BCr_Nicht-Freaks:_Sitemap&action=raw')
+class TOC
+  def initialize( headline, location, children )
+    @headline = headline
+    @location = location
+    @children = children
+  end
 
-sitemap = Net::HTTP.get(sitemap_url)
+  def to_book()
+    # TODO
+  end
+end
 
-puts sitemap.scan(/\[\[.*\]\]/)
+class Book
+  # TODO
+
+  def to_tex()
+    # TODO
+  end
+end
+
+Sitemap_Booktitle = /==\W*\[\[(.*)\|(.*)\]\]\W*==/
+Sitemap_Chapter = /===\W*(.*)\W*===/
+
+def fetch( item )
+  sitemap = 'https://de.wikibooks.org/w/index.php'
+  what = URI.escape(item, /[^a-zA-Z\d\-._~!$&\'()*+,;=:@\/]/)
+  url = URI( sitemap + '?' + what + '&action=raw' )
+  Net::HTTP.get( url ).force_encoding("UTF-8")
+end
+
+# A crude heuristic to turn a wikibooks page to several TOC (table of
+# contents) objects.
+def wikipage_to_tocs( item )
+  # TODO
+  array = fetch(item).split(Sitemap_Booktitle)
+end
+
+tocs = wikipage_to_tocs('title=Mathe für Nicht-Freaks: Sitemap')
+tocs.each { |toc| toc.to_book.to_tex }
