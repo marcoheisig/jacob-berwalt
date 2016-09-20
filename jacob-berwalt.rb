2c90ee9c250313846b0ab11286484ab81de8abad
Marco Heisig <marco.heisig@fau.de>
2016-09-20 10:37:09 +0200

    Book objects with Ruby awesomeness

--- jacob-berwalt.rb
+++ jacob-berwalt.rb
@@ -4,20 +4,33 @@
 require "open-uri"
 require "net/http"
 
-class TOC
-  def initialize( headline, location, children )
-    @headline = headline
-    @location = location
-    @children = children
-  end
-
-  def to_book()
-    # TODO
-  end
-end
-
 class Book
-  # TODO
+  def initialize(title, tocdepth = 2)
+    @title = title
+    @tocdepth = tocdepth
+    @tree = [title, "", []]
+  end
+
+  def add_node(title, body, level)
+    tree = @tree
+    for i in 1..level do
+      tree = tree[2].last
+    end
+    tree[2].push [title, body, []]
+    self
+  end
+
+  def add_chapter(title, body)
+    self.add_node(title, body, 0)
+  end
+
+  def add_section(title, body)
+    self.add_node(title, body, 1)
+  end
+
+  def to_s()
+    "#<Book " + @tree.to_s + ">"
+  end
 
   def to_tex()
     # TODO
@@ -36,10 +49,11 @@ end
 
 # A crude heuristic to turn a wikibooks page to several TOC (table of
 # contents) objects.
-def wikipage_to_tocs( item )
+def wikipage_to_books( item )
   # TODO
   array = fetch(item).split(Sitemap_Booktitle)
 end
 
-tocs = wikipage_to_tocs('title=Mathe für Nicht-Freaks: Sitemap')
-tocs.each { |toc| toc.to_book.to_tex }
+#books = wikipage_to_books('title=Mathe für Nicht-Freaks: Sitemap')
+# books.each { |book| book.to_tex }
+puts Book.new("TITLE").add_chapter("Chapter 1", "foo").add_section("Section 1", "bla").add_chapter("Chapter 2", "bar")
